515401e3ed746816d7a4a811bf7d1b059d60515f
lidonil <jinmi16@gmail.com>
2018-08-04 16:39:29 -0500

    local storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit , DoCheck } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, DoCheck  {
   title = 'app';
+  emailContacto: string;
+
+  ngOnInit() {
+    console.log(' desde app componente');
+    console.log(localStorage.getItem('emailContato'));
+    this.emailContacto = localStorage.getItem('emailContato');
+    console.log('cargo email del localstorage: ' + this.emailContacto);
+  }
+  ngDoCheck() {
+    console.log(' desde app componente');
+    // para que actualise automaticamete sin recargar la pagina el email que se guarla en localstorage cuando se le ha cambiado
+    this.emailContacto = localStorage.getItem('emailContato');
+    console.log(localStorage.getItem('emailContato'));
+
+  }
+
 }
--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-
+  public emailContacto: string;
   constructor() { }
 
   ngOnInit() {
+  }
+  guardarEmail() {
+    //  console.log(this.emailContacto);
+    // guardamos en localstorage
+    localStorage.setItem('emailContato', this.emailContacto);
+    // recupero del localstorage:
+    // console.log(localStorage.getItem('emailContato'));
+
   }
 
 }
