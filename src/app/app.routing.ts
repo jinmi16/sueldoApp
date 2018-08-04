33efbf67ea8f683738f01e55673469828e5ff7c6
lidonil <jinmi16@gmail.com>
2018-08-04 16:15:25 -0500

    routes2

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,19 +1,17 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 // importamops nuestros componentes
+import { HomeComponent } from './components/home/home.component';
 import { TiendaComponent } from './components/tienda/tienda.component';
 import { AnimalsComponent } from './components/animals/animals.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { KeeperComponent } from './components/keeper/keeper.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 
-
-
-
-
 const appRoutes: Routes = [
-  { path: '', component: TiendaComponent },
-  { path: '', redirectTo: 'tienda', pathMatch: 'full' },
+  { path: '', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
   { path: 'tienda', component: TiendaComponent },
   { path: 'animales', component: AnimalsComponent },
   { path: 'contacto', component: ContactComponent },
