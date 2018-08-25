53bb614991f515753faf6f1b6cab69b47ce9e319
lidonil <jinmi16@gmail.com>
2018-08-25 15:13:22 -0500

    login style

--- src/app/components/animals/animals.component.ts
+++ src/app/components/animals/animals.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./animals.component.css']
 })
 export class AnimalsComponent implements OnInit {
-
+  name = 'Angular 6';
+  marked = false;
+  theCheckbox = false;
+  public arraycheck = [
+    { id: '01', name: 'matematica' },
+    { id: '02', name: 'Lenguaje' }
+  ];
   constructor() { }
 
   ngOnInit() {
   }
+  toggleVisibility(e, id) {
+    console.log(JSON.stringify(e.target) + ' | ' + id);
+    this.marked = e.target.checked;
+  }
+
 
 }
